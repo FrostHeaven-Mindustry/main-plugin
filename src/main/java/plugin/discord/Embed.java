e44aab8961bb865a8d4a38e61fe22082bbfc83b4
Волшебник <127457914+VikoMin@users.noreply.github.com>
2025-04-04 03:23:12 +0700

    Update Embed.java

--- src/main/java/plugin/discord/Embed.java
+++ src/main/java/plugin/discord/Embed.java
@@ -2,11 +2,9 @@ package plugin.discord;
 
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.entity.permission.Role;
-import plugin.database.wrappers.PlayerData;
-import plugin.database.wrappers.getIdBySnowFlake;
+import plugin.database.wrappers.PlayerData.*;
 import plugin.Utilities;
 import useful.Bundle;
-
 import java.awt.*;
 import java.time.Duration;
 import java.util.Optional;
